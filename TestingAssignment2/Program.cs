94f73084a24a569ea1202f96883522a9da4e9be9
Vrunda savaliya <71866329+vrunda-vs@users.noreply.github.com>
2021-04-10 12:12:03 +0530

    Update Program.cs

--- TestingAssignment2/Program.cs
+++ TestingAssignment2/Program.cs
@@ -17,21 +17,31 @@ namespace TestingAssignment2
                 Console.WriteLine("\"" + number + "\" can not convert in numeric type");
             else
                 Console.WriteLine("\"" + number + "\" numeric value is : " + number.ConvertStringToNumber());
+            
+            //convert into uppercase
             Console.WriteLine("Input : \"" + input + "\" into Uppercase Letter : " + input.ConvertToUpper());
+             //convert into lowercase
             input = "VRUNDA";
             Console.WriteLine("Input : \"" + input + "\" into Lowercase Letter : " + input.ConvertToLower());
-            input = "vrunda savaliya";
-            Console.WriteLine("Convert Input : \"" + input + "\" into oppsite Letter : " + input.ConvertToOppositeCase());
+             //convert into oppositecase
             input = "VRUndA savaLIya";
+            Console.WriteLine("Convert Input : \"" + input + "\" into oppsite Letter : " + input.ConvertToOppositeCase());
+            //convert into titlecase
+            input = "VRUNDA";
             Console.WriteLine("Input : \"" + input + "\" into Titlecase Letter : " + input.ConvertToTitleCase());
+            //check wherther string is in lowercase or not
             input = "vrunda";
             Console.WriteLine("Check all the character of string is lower case or not : \"" + input + "\" : " + input.IsLower());
+            //convert into capitlize
             input = "vrunda savaliya";
             Console.WriteLine("Input : \"" + input + "\" into Capitalize : " + input.ConvertToCapitalize());
+            //check whether string is uppercase  or not
             input = "VRUNDA";
             Console.WriteLine("Is input in uppercase : \"" + input + "\" : " + input.IsUpper());
+            //countingword
             input = "vrunda savaliya";
             Console.WriteLine("No of words in the input : \"" + input + "\" : " + input.CountingWord());
+            //removing last charecter 
             input = "Vrunda Savaliya";
             Console.WriteLine("Removing last character   : \"" + input + "\" : " + input.RemoveLastCharacterFrom());
             Console.WriteLine("==================================================");
